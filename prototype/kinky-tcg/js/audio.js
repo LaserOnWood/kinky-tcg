9eadcd59b4df5c4620e0d83be371119b4c364015
toeiazarothis <laseronwood@gmail.com>
2026-09-06 14:47:40 +0200

    Update audio.js

--- prototype/kinky-tcg/js/audio.js
+++ prototype/kinky-tcg/js/audio.js
@@ -9,11 +9,13 @@ const AUDIO_MUTED_STORAGE_KEY = "kinky_tcg_audio_muted";
 
 const sons = {
   bonneReponse: new Audio("assets/audio/bonne-reponse.mp3"),
-  erreur: new Audio("assets/audio/erreur.mp3")
+  erreur: new Audio("assets/audio/erreur.mp3"),
+  easter : new Audio("assets/audio/suprise.mp3")
 };
 
-sons.bonneReponse.volume = 0.6;
+sons.bonneReponse.volume = 0.45;
 sons.erreur.volume = 0.45;
+sons.easter.volume = 0.45;
 
 let sonsDesactives = chargerPreferenceAudio();
 
